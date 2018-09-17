e25395cfac694a2b63b6b716f4688c0f1ea396a8
aminkhavariionic <43031205+aminkhavariionic@users.noreply.github.com>
2018-09-17 14:17:37 +0430

    1

--- src/app/main.ts
+++
@@ -1,9 +0,0 @@
-// Project Name: IonicEcommerce
-// Project URI: http://ionicecommerce.com
-// Author: VectorCoder Team
-// Author URI: http://vectorcoder.com/
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
